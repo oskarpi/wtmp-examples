2af1c20805c4cac63e48fdc7994893e5d94affb7
35840 <osku1993@luukku.com>
2021-01-25 16:09:15 +0200

    'aloitus'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('Hello console!');
+'use strict';
+
+const touchButton = document.querySelector('button');
+const body = document.querySelector('body');
+
+/*
+i.
+
+ */
+document.addEventListener('keydown', (event)=>{
+
+});
+
+/*
+ii.
+
+ */
+document.addEventListener('dblclick', (event)=>{
+  console.log('clicked coordinates: ', event.pageX, event.pageY);
+});
+
+
+/*
+iii.
+
+ */
+touchButton.addEventListener('mouseover', () =>{
+ console.log('you touched the button');
+});
+
+
+/*
+iv.
+ */
+
+setTimeout(() => {
+  const warning = document.createElement('h1');
+  warning.textContent = "Hurry up";
+  body.append(warning);
+}, 15000);
+
+/*
+v
+ */
+
