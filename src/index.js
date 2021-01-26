18575f103800c72126cc7ad4973a0ac11345e918
35840 <osku1993@luukku.com>
2021-01-26 11:35:41 +0200

    'valmis'

--- src/index.js
+++ src/index.js
@@ -3,35 +3,52 @@
 const touchButton = document.querySelector('button');
 const body = document.querySelector('body');
 
-/*
-i.
 
- */
-document.addEventListener('keydown', (event)=>{
+//i.
+
+
+let secretWord = 'hello';
+let input = '';
+window.addEventListener('keypress', function(event) {
+  input += event.key;
+
+  if(input.length > secretWord.length) {
+    input = input.substr(1);
+  }
+
+  if(input === secretWord) {
+    alert("SECRET CODE!");
+  }
 
 });
 
-/*
-ii.
 
- */
-document.addEventListener('dblclick', (event)=>{
-  console.log('clicked coordinates: ', event.pageX, event.pageY);
+
+//ii.
+
+const doubleClickedCoords = (event) =>{
+  event.preventDefault();
+  console.log(`clicked coordinates:, ${event.pageX}, ${event.pageY}`);
+};
+
+document.addEventListener('dblclick', doubleClickedCoords);
+
+
+//iii.
+
+touchButton.addEventListener('mouseover', (event) =>{
+  event.preventDefault();
+  touchButton.textContent = 'you touched the button';
+});
+
+touchButton.addEventListener('mouseleave', (event) =>{
+  event.preventDefault();
+  touchButton.textContent = 'Touch me';
 });
 
 
-/*
-iii.
+//iv.
 
- */
-touchButton.addEventListener('mouseover', () =>{
- console.log('you touched the button');
-});
-
-
-/*
-iv.
- */
 
 setTimeout(() => {
   const warning = document.createElement('h1');
@@ -39,7 +56,48 @@ setTimeout(() => {
   body.append(warning);
 }, 15000);
 
-/*
-v
- */
+
+//v.
+
+let idleHurry;
+const idleWarning = document.getElementById('idle-warning');
+
+const setIdleTimer = () => {
+  idleHurry = setTimeout(() =>{
+    idleWarning.textContent = "Hurry up, idle warning";
+    body.append(idleWarning);
+  }, 15000);
+};
+
+setIdleTimer();
+
+window.addEventListener('mousemove',  () =>{
+  idleWarning.textContent = '';
+  clearTimeout(idleHurry);
+  setIdleTimer();
+});
+
+window.addEventListener('keypress', () =>{
+  idleWarning.textContent = '';
+  clearTimeout(idleHurry);
+  setIdleTimer();
+});
+
+window.addEventListener('click', () => {
+  idleWarning.textContent = '';
+  clearTimeout(idleHurry);
+  setIdleTimer();
+});
+
+window.addEventListener('dblclick', ()=> {
+  idleWarning.textContent = '';
+  clearTimeout(idleHurry);
+  setIdleTimer();
+});
+
+
+
+
+
+
 
